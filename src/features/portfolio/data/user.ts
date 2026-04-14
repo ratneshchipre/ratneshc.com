1c067e28c3f2553f80a7d87914606a3adb81ec9a
ratneshchipre <ratneshchipre@gmail.com>
2026-04-14 23:17:45 +0530

    feat: add user data structure and type definitions

---
+++ src/features/portfolio/data/user.ts
@@ -0,0 +1,30 @@
+import type { User } from "@/features/portfolio/types/user";
+
+export const USER: User = {
+  firstName: "Ratnesh",
+  lastName: "Chipre",
+  displayName: "Ratnesh",
+  username: "ratneshchipre",
+  address: "Maharashtra, India",
+  website: "https://ratneshc.com",
+  jobTitle: "Full-Stack Web Developer",
+  about: `
+- I'm a 20yo Full-Stack Developer focused on building clean, scalable web products.
+- I love crafting elegant web experiences, shipping fast, and turning ideas into real products.
+  `,
+  avatar: "https://ratneshc.com/images/avatar.png",
+  ogImage: "https://ratneshc.com/images/opengraph-image.png",
+  timeZone: "Asia/Kolkata",
+  keywords: [
+    "ratnesh",
+    "ratneshc.com",
+    "ratnesh chipre",
+    "ratneshc portfolio",
+    "ratnesh portfolio",
+    "web developer",
+    "frontend engineer",
+    "ratneshchipre",
+    "typescript",
+    "next.js",
+  ],
+};
---
+++ src/features/portfolio/types/user.ts
@@ -0,0 +1,14 @@
+export type User = {
+  firstName: string;
+  lastName: string;
+  displayName: string;
+  username: string;
+  address: string;
+  website: string;
+  jobTitle: string;
+  about: string;
+  avatar: string;
+  ogImage: string;
+  keywords: string[];
+  timeZone: string;
+};
