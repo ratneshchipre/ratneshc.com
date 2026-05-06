8e6f98606001783060c443b4f0557f5978524220
ratneshchipre <ratneshchipre@gmail.com>
2026-05-06 14:05:36 +0530

    feat: add scroll to top component to app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,11 @@
+import dynamic from "next/dynamic";
+
 import SiteFooter from "@/components/site-footer";
 import SiteHeader from "@/components/site-header";
+
+const ScrollToTop = dynamic(() =>
+  import("@/components/scroll-to-top").then((mod) => mod.ScrollToTop)
+);
 
 export default async function AppLayout({
   children,
@@ -11,6 +17,7 @@ export default async function AppLayout({
       <SiteHeader />
       <main className="overflow-hidden px-5">{children}</main>
       <SiteFooter />
+      <ScrollToTop />
     </>
   );
 }
